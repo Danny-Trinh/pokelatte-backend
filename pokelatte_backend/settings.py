30266c2005152556eb894e03f738bdc691a14adc
unknown <dtrinh403@yahoo.com>
2020-07-12 20:27:16 -0500

    heroku addons

--- pokelatte_backend/settings.py
+++ pokelatte_backend/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# if os.environ.get("NO_DEBUG"):
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'home'
