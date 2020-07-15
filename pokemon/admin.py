76ca84dd5c4830ce22bae30bf6fb945b5eee2692
unknown <dtrinh403@yahoo.com>
2020-07-14 21:40:48 -0500

    added pokemon model to admin view

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import PokemonPost
+from .models import Pokemon
 
 # Register your models here.
-admin.site.register(PokemonPost)
+admin.site.register(Pokemon)
