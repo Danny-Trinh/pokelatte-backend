9b04293bcd42473b88f2abcc254516a19925d9e7
unknown <dtrinh403@yahoo.com>
2020-07-25 00:09:57 -0500

    added simple jwt

--- pokemon/serializer.py
+++ pokemon/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import PokemonPost, Pokemon
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.settings import api_settings
 from django.contrib.auth.models import User
 
 
@@ -8,11 +9,20 @@ class UserSerializer(serializers.ModelSerializer):
     """
     User Creation Serializer
     """
+    token = serializers.SerializerMethodField()
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ('token', 'username', 'password')
+
+    def get_token(self, obj):
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(obj)
+        token = jwt_encode_handler(payload)
+        return token
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
