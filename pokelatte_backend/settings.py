2b906cf7b31a6e4578af6f474b7949e7eb7d8083
unknown <dtrinh403@yahoo.com>
2020-08-08 14:17:13 -0500

    adding better cors whitelist

--- pokelatte_backend/settings.py
+++ pokelatte_backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["127.0.0.1", "pokelatte-backend.herokuapp.com"]
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "https://pokelatte.herokuapp.com",
+    "pokelatte.herokuapp.com"
 ]
 
 
